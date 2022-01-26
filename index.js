7a4217c27b013ba6aedf28407cb3e555a37b822f
Rav <38435107+Ravalous@users.noreply.github.com>
2022-01-26 16:28:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+// Libs
+const crypto = require("crypto")
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
+const { uniqueNamesGenerator, adjectives, colors, animals, countries, names, languages, starWars } = require('unique-names-generator')
+const { PuppeteerBlocker } = require('@cliqz/adblocker-puppeteer')
+const {fetch} = require('cross-fetch')
+const fs = require('fs')
+const { Console } = require('console')
+
+// rest 
